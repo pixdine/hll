69d50cd9fdecf1d367c4fd91b9536c188875a93f
yunyoungseo <yunyoungseo@dfluid.kr>
2023-08-23 11:57:06 +0900

    아코디언 기능 추가

--- membership/resources/js/ui.common.js
+++ membership/resources/js/ui.common.js
@@ -66,6 +66,19 @@ $(document).ready(function () {
         }
     }
     $(document).tooltip();
+
+    $.fn.accordion = function () {
+        this.each(function(index, element) {
+            $("[data-accordion-header]", element).click(function(e){
+                e.preventDefault()
+                const contentEl = $(`[data-accordion-content="${this.dataset.accordionHeader}"]`, element)
+                const expanded = $(this).hasClass('is-expanded')
+                $(this).toggleClass('is-expanded', !expanded)
+                contentEl.css("max-height", expanded?0:contentEl[0]?.scrollHeight)
+            })
+        })
+    }
+    $("[data-accordion]").accordion();
 });
 
 function showLayer(target) {

bb2e5285dd084104cfde0d47cf2cbd64c9c338cc
ju youngnam <inno76@gmail.com>
2023-09-02 15:18:28 +0900

    달력 수정

--- magazine_bazaar/resources/js/ui.common.js
+++ magazine_bazaar/resources/js/ui.common.js
@@ -32,19 +32,30 @@ $(document).ready(function(){
     });
 
 	/* datepicker */
-	var datepicker = $(".datepicker");
-	if(datepicker.length > 0) {
-		datepicker.datepicker({
-			beforeShow: function (input, inst) {
-				setTimeout(function () {
-					inst.dpDiv.css({
-						top: datepicker.offset().top + 35
-					});
-				}, 0);
-			}
-		}); 
-	}
+	$.datepicker.setDefaults({
+		showOn: "both",
+		dateFormat: 'yymmdd',
+		prevText: '이전 달',
+		nextText: '다음 달',
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		showMonthAfterYear: true,
+		yearSuffix: '년',
+		beforeShow: function (input, inst) {
+			setTimeout(function () {
+			inst.dpDiv.css({
+				top: datepicker.offset().top + datepicker.height()
+			});
+			}, 0);
+		}
+	});
 
+	$(window).on('resize',function() {
+		$(".datepicker").datepicker("hide");
+	})
 
 	/* popup open */
 	$('[data-popup-open]').on('click', function(e) {
